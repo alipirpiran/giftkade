933ce434efda3aa24951c1faa3aabe94a931b091
Ali Pirpiran <alipirpiran@gmail.com>
2019-12-28 18:56:06 +0330

    add authenticate check for users

---
+++ auth/user.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+    const authToken = req.header('auth-token');
+    if (!authToken) return res.status(403).send({ error: { message: 'access denied' } });
+
+    try {
+        const payload = jwt.verify(authToken, process.env.TOKEN_SECRET);
+        req.user = payload._id;
+        next()
+    } catch (error) {
+        return res.status(400).send({ error: { message: 'wrong token' } })
+    }
+}
