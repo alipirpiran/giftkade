74ccfe9d6b104e952c690593c8bf12123da3b3ea
Ali Pirpiran <alipirpiran@gmail.com>
2019-12-28 18:55:35 +0330

    add login function

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcryptjs')
+const joi = require('joi')
+const jwt = require('jsonwebtoken')
+const router = require('express').Router();
+
+const User = require('../models/user')
+
+router.post('/login', async (req, res) => {
+    const info = req.body;
+
+    const { error } = validateLogin(info);
+    if (error) return res.status(400).send({ error: { messsage: 'ورودی هارا کنترل کنید.' } })
+
+    const user = await User.findOne({ email: info.email })
+    if (!user) return res.status(400).send({ error: { messsage: 'ایمیل یا رمز عبور اشتباه است.' } })
+
+    const isValidPass = await bcrypt.compare(info.password, user.password);
+    if (!isValidPass) return res.status(400).send({ error: { messsage: 'ایمیل یا رمز عبور اشتباه است.' } });
+
+    // create and assign a token
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+});
+
+function validateLogin(data) {
+    return joi.validate(data, {
+        email: joi.string().email().required(),
+        password: joi.string().min(8).required()
+    })
+}
+
+module.exports = router;
