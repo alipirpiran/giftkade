aed2edbe791e62e4610e45948e2fb01fd5898edb
Ali Pirpiran <alipirpiran@gmail.com>
2020-01-08 11:40:59 +0330

    add admin auth

---
+++ auth/admin.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+
+module.exports = async (req, res, next) => {
+    const authToken = req.header('auth-token');
+    if (!authToken) return res.status(403).send({ error: { message: 'شما دسترسی ندارید' } });
+
+    try {
+        const payload = jwt.verify(authToken, process.env.TOKEN_SECRET);
+        if (payload._id) {
+            const user = await User.findById(payload._id);
+            if (user && user.isAdmin) {
+                req.user = payload._id;
+                next()
+            }
+        }
+
+        return res.status(400).send({ error: { message: 'شما دسترسی ندارید' } })
+
+    } catch (error) {
+        return res.status(400).send({ error: { message: 'شما دسترسی ندارید' } })
+    }
+}
