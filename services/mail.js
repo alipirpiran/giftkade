de5e246395c58eb4c10cf10f86f1a0f8a594ba12
Ali Pirpiran <alipirpiran@gmail.com>
2020-01-21 23:05:08 +0330

    adding email service

---
+++ services/mail.js
@@ -0,0 +1,34 @@
+const nodemailder = require('nodemailer')
+
+const service = process.env.MAIL_SERVICE || 'gmail'
+const username = process.env.MAIL_USERNAME;
+const password = process.env.MAIL_PASSWORD;
+
+const transporter = nodemailder.createTransport({
+    service,
+    auth: {
+        user: username,
+        pass: password
+    }
+})
+
+module.exports.sendMail = (receiver, subject, html) => {
+    var mailOptions = {
+        from: username,
+        to: receiver,
+        subject,
+        html
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
+}
+
+module.exports.giftCardHTML = () => {
+
+}
