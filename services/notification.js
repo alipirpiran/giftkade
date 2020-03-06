ce387541a12de3fadb2a1214b4a495e9170c5ab2
Ali Pirpiran <alipirpiran@gmail.com>
2020-03-06 23:55:28 +0330

    fix url for notification

--- services/notification.js
+++ services/notification.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch').default;
 const BOT_PORT = process.env.BOT_PORT;
 
 module.exports.newUserNotification = (id, mobile, date) => {
-    fetch(`localhost:${BOT_PORT}/newUser`, {
+    fetch(`http://localhost:${BOT_PORT}/newUser`, {
         method: 'POST',
         body: {
             id,
@@ -13,7 +13,7 @@ module.exports.newUserNotification = (id, mobile, date) => {
 };
 
 module.exports.newOrder = (id, title, totalAmount, count) => {
-    fetch(`localhost:${BOT_PORT}/newOrder`, {
+    fetch(`http://localhost:${BOT_PORT}/newOrder`, {
         method: 'POST',
         body: {
             id,
