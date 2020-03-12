86d8045a9bd3d6c318cb045fc2681b2aa39ba76d
Ali Pirpiran <alipirpiran@gmail.com>
2020-03-12 11:02:29 +0330

    all send methods here, email, sms

---
+++ services/sendService.js
@@ -0,0 +1,25 @@
+const mailService = require('./mail');
+const giftcardService = require('./giftcardService');
+
+async function sendGiftcards({order, user}) {
+    const codes = [];
+    for (const item of order.finalGiftcards) {
+        const giftcard = await Token.findById(item);
+        const code = giftcardService.decryptToken(giftcard.code);
+        codes.push(code);
+    }
+
+    if (order.targetType == 'email') {
+        var email = order.target ? order.target : user.email;
+        var html = await mailService.giftCardHTML(order, codes);
+        mailService.sendMail(email, 'گیفت کارت های خریداری شده', html);
+    } else if (order.targetType == 'sms') {
+        var mobile = order.target ? order.target : user.phoneNumber;
+
+        // TODO send to sms
+    }
+}
+
+module.exports = {
+    sendGiftcards
+}
