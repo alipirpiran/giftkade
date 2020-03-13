ac0a7f06f9b581ea10dbf30bffdd1630d64534aa
Ali Pirpiran <alipirpiran@gmail.com>
2020-03-13 22:40:13 +0330

    add new configs

--- db/index.js
+++ db/index.js
@@ -5,6 +5,10 @@ const debug = require('debug')('giftShop:DB');
 const URL = process.env.DB_URL || 'mongodb://localhost/giftShop';
 
 mongoose
-    .connect(URL, { useFindAndModify: false })
+    .connect(URL, {
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => debug('connected to mongodb'))
     .catch(err => debug(err));
