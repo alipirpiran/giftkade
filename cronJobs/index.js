9211c70c1f380f514d6325ed8f9bfdfe829e95af
Ali Pirpiran <alipirpiran@gmail.com>
2020-04-23 00:54:32 +0430

    fix timing

--- cronJobs/index.js
+++ cronJobs/index.js
@@ -2,6 +2,6 @@ const CronJob = require('cron').CronJob;
 const cleanUpTokens = require('./cleanUpTokens');
 
 // check db for pending tokens every 20 min.
-const job = new CronJob('* 20 * * * *', cleanUpTokens, null, true);
+const job = new CronJob('* */20 * * * *', cleanUpTokens, null, true);
 
 job.start();
