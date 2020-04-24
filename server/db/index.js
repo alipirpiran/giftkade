abc30c6e2b94ddd76f6591fe9761545c7dd94411
Ali Pirpiran <alipirpiran@gmail.com>
2020-04-24 23:11:18 +0430

    add support for db auth

--- server/db/index.js
+++ server/db/index.js
@@ -4,11 +4,17 @@ const debug = require('debug')('giftShop:DB');
 
 const URL = process.env.DB_URL || 'mongodb://localhost/giftShop';
 
+const options = {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+if (process.env.NODE_ENV == 'production') {
+    options.user = process.env.DB_USER;
+    options.pass = process.env.DB_PASS;
+}
+
 mongoose
-    .connect(URL, {
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(URL, options)
     .then(() => debug('connected to mongodb'))
-    .catch(err => debug(err));
+    .catch((err) => debug(err));
