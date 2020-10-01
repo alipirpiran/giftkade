7cf1138d2d8fbb54d145f8b9530c02384e932b97
Ali Pirpiran <alipirpiran@gmail.com>
2020-10-01 19:52:33 +0330

    chages

--- cronJobs/index.js
+++ cronJobs/index.js
@@ -1,6 +1,8 @@
 const CronJob = require('cron').CronJob;
 const cleanUpTokens = require('./cleanUpTokens');
 
+console.log('Started');
+
 // check db for pending tokens every 20 min.
 const job = new CronJob('* */20 * * * *', cleanUpTokens, null, true);
 
