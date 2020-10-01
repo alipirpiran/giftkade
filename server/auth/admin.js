8f6a6c5f5f9f18787b48d3112f361eb518d22e6a
Ali Pirpiran <alipirpiran@gmail.com>
2020-10-01 19:42:53 +0330

    change way check if request is from localhost

--- server/auth/admin.js
+++ server/auth/admin.js
@@ -6,35 +6,39 @@ const LOCAL_AUTH_TOKEN = process.env.LOCAL_AUTH_TOKEN;
 const User = require('../models/user');
 
 module.exports = async (req, res, next) => {
-    // return next()
-    const authToken = req.header('auth-token');
-    if (!authToken) return next(Errors.forbidden());
+  // return next()
+  const authToken = req.header('auth-token');
+  if (!authToken) return next(Errors.forbidden());
 
-    if (isReqFromLocalhost(req)) return next();
+  if (isReqFromLocalhost(req)) return next();
 
-    try {
-        const payload = jwt.verify(authToken, process.env.TOKEN_SECRET);
-        
-        if (!payload.exp || payload.exp - Date.now() / 1000 < 0)
-            return next(Errors.forbidden());
+  try {
+    const payload = jwt.verify(authToken, process.env.TOKEN_SECRET);
 
-        if (payload._id) {
-            const user = await User.findById(payload._id);
-            if (user && user.isAdmin && user.isActive) {
-                req.user = payload._id;
-                return next();
-            }
-        }
+    if (!payload.exp || payload.exp - Date.now() / 1000 < 0)
+      return next(Errors.forbidden());
 
-        return next(Errors.forbidden());
-    } catch (error) {
-        return next(Errors.forbidden());
+    if (payload._id) {
+      const user = await User.findById(payload._id);
+      if (user && user.isAdmin && user.isActive) {
+        req.user = payload._id;
+        return next();
+      }
     }
+
+    return next(Errors.forbidden());
+  } catch (error) {
+    return next(Errors.forbidden());
+  }
 };
 
 let isReqFromLocalhost = function (req) {
-    if (!LOCAL_AUTH_TOKEN) return false;
+  // if (!LOCAL_AUTH_TOKEN) return false;
 
-    if (req.headers['auth-token'] == LOCAL_AUTH_TOKEN) return true;
+  // if (req.headers['auth-token'] == LOCAL_AUTH_TOKEN) return true;
+  // return false;
+
+  if (req.headers['x-real-ip']) {
     return false;
+  } else return true;
 };
