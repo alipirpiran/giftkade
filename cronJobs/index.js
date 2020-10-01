2b657603a46fde2f4fb4a2c4aba290ec214c0ce8
Ali Pirpiran <alipirpiran@gmail.com>
2020-10-01 19:53:32 +0330

    changes

--- cronJobs/index.js
+++ cronJobs/index.js
@@ -4,6 +4,7 @@ const cleanUpTokens = require('./cleanUpTokens');
 console.log('Started');
 
 // check db for pending tokens every 20 min.
+cleanUpTokens();
 const job = new CronJob('* */20 * * * *', cleanUpTokens, null, true);
 
 job.start();
