5355c1f07f2f54c5f6a4c28059b0452cd9672346
Ali Pirpiran <alipirpiran@gmail.com>
2020-10-01 19:12:09 +0330

    use service, host

--- server/services/mail.js
+++ server/services/mail.js
@@ -2,70 +2,72 @@ const nodemailder = require('nodemailer');
 const debug = require('debug')('giftkade:mailService');
 const ejs = require('ejs');
 
-const service = process.env.MAIL_SERVICE;
+const service = process.env.MAIL_SERVICE || '';
+const host = process.env.MAIL_HOST || '';
 const username = process.env.MAIL_USERNAME;
 const password = process.env.MAIL_PASSWORD;
 
 const BASE_URL = process.env.BASE_URL;
 
 const transporter = nodemailder.createTransport({
-    host: service,
-    port: 587,
-    secure: false,
-    auth: {
-        user: username,
-        pass: password,
-    },
+  host,
+  service,
+  port: 587,
+  secure: false,
+  auth: {
+    user: username,
+    pass: password,
+  },
 });
 
 module.exports.sendMail = (receiver, subject, html) => {
-    var mailOptions = {
-        from: `Giftkade <${username}>`,
-        to: receiver,
-        subject,
-        html,
-    };
+  var mailOptions = {
+    from: `Giftkade <${username}>`,
+    to: receiver,
+    subject,
+    html,
+  };
 
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            debug(err);
-        } else {
-            // console.log('Email sent: ' + info.response);
-        }
-    });
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      debug(err);
+    } else {
+      // console.log('Email sent: ' + info.response);
+    }
+  });
 };
 
 module.exports.shopHTML = async (order, codes, payment) => {
-    var html = '';
-    var product_id = order.product;
-    for (const code of codes) {
-        html +=
-            (await giftcardHTML(
-                order.title,
-                order.description,
-                code,
-                product_id
-            )) + '\n';
-    }
-    return _getHTML(html, { orderid: order.orderid, refid: payment.refId, totalPrice: payment.amount });
+  var html = '';
+  var product_id = order.product;
+  for (const code of codes) {
+    html +=
+      (await giftcardHTML(order.title, order.description, code, product_id)) +
+      '\n';
+  }
+  return _getHTML(html, {
+    orderid: order.orderid,
+    refid: payment.refId,
+    totalPrice: payment.amount,
+  });
 };
 
 // TODO: design giftcard html
 async function giftcardHTML(title, description, code, product_id) {
-    const imageUrl = `${BASE_URL}/uploads/${product_id}`;
+  const imageUrl = `${BASE_URL}/uploads/${product_id}`;
 
-    return ejs.renderFile(__dirname + '/../views/templates/giftcard.ejs', {
-        imageUrl,
-        code,
-        title,
-    });
+  return ejs.renderFile(__dirname + '/../views/templates/giftcard.ejs', {
+    imageUrl,
+    code,
+    title,
+  });
 }
 
 function _getHTML(giftcardsElements, { orderid, refid, totalPrice }) {
-    return ejs.renderFile(__dirname + '/../views/templates/shop.ejs', {
-        giftcards: giftcardsElements,
-        orderid,
-        refid,
-        totalPrice
-    });
+  return ejs.renderFile(__dirname + '/../views/templates/shop.ejs', {
+    giftcards: giftcardsElements,
+    orderid,
+    refid,
+    totalPrice,
+  });
 }
